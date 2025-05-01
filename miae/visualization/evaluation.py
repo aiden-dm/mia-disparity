c470f6643693be3e7d80d4bd798c3cb052550c9c
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2025-04-30 17:06:37 -0700

    initial commit

---
+++ miae/visualization/evaluation.py
@@ -0,0 +1,3 @@
+# this is placeholder that will be filled with all investigation methods we implement and use.
+# for example, frequency analysis, metric correlation, etc.
+# it is expected that each investigation method will takes attack scenarios(include target model, attack methods,  or/ and the metrics as input.
